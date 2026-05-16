49659c958917a04ca9db8c5f9432d29a04142d3b
KAVIYA S <147968088+KaviyaSivakumar08@users.noreply.github.com>
2026-05-16 12:19:41 +0530

    Add files via upload

---
+++ qu1.py
@@ -0,0 +1,6 @@
+num=list(map(int,input().split()))
+num.sort()
+
+print(num)
+print(num[-2:-1])
+
---
+++ qu3.py
@@ -0,0 +1,8 @@
+str=input()
+str1=str.split()
+print(str1)
+
+
+i=str1[0]
+if i in str1:
+    print(i)
